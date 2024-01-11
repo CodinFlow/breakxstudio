0b68b9e8fb8c7ab91d25e7e206a48c081a7c10fd
Youssef Mjahed <contact.flexible@gmail.com>
2024-01-11 14:49:39 +0100

    update

--- send-email.php
+++ send-email.php
@@ -1,31 +1,33 @@
 <?php
 
-$name = $_POST["name"];
-$email = $_POST["email"];
+$fname = $_POST["fname"];
+$lname = $_POST["lname"];
+$email = $_POST["c_email"];
 $subject = $_POST["subject"];
 
 
-require "vendor/autoload.php";
 
+require "vendor/autoload.php";
 use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
+
 
 $mail = new PHPMailer(true);
 
 // $mail->SMTPDebug = SMTP::DEBUG_SERVER;
-
 $mail->isSMTP();
+$mail->CharSet = 'UTF-8';
+$mail->IsHTML(true);
 $mail->SMTPAuth = true;
-
+$mail->Subject = "breakXstudio Kontakt";
 $mail->Host = "smtp.gmail.com";
 $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
 $mail->Port = 587;
 
 $mail->Username = "contact.flexible@gmail.com";
-$mail->Password = "password";
+$mail->Password = "xxxxxx";
 
-$mail->setFrom($email, $name);
-$mail->addAddress("contact.flexible@gmail.com", "BreakXStudio");
+$mail->setFrom($email, $fname . " " . $lname . " | " . $email);
+$mail->addAddress("youssef.mjahed@outlook.de", "breakXstudio");
 
 
 $mail->Body = $subject;
